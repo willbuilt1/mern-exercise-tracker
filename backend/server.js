861764e0b9ba3476fe7c7cae875fc2722c3fc37b
willbuilt1 <wfoster12@gmail.com>
2020-12-12 14:06:46 +0000

    CRUD operations complete

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
-require('dotenv').config;
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,9 +10,29 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.listen(port, () =>{
-    console.log(`server on ${port}`);
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
 });
 
+const connection = mongoose.connection;
 
+connection.once('open', () => {
+  console.log('DB connected');
+});
 
+const userRouter = require('./routes/users');
+const exerciseRouter = require('./routes/exercises');
+
+app.use('/users', userRouter);
+app.use('/exercises', exerciseRouter);
+
+app.use('/', (req, res) => {
+  res.send('Hello');
+});
+
+app.listen(port, () => {
+  console.log(`server on ${port}`);
+});
